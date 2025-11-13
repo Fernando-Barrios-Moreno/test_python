0c192b7c846e632a9df3d9461f0fd6ca709107d6
Fernando-Barrios-Moreno <fbarriosmoreno9040@floridapoly.edu>
2025-11-13 04:00:03 +0000

    Player Imput Implemented

--- game.py
+++ game.py
@@ -1,1 +1,8 @@
 print("Welcome to Rock-Paper-Scissors!")
+
+player_choice = input("Enter your choice (rock, paper, or scissors): ").lower()
+
+if player_choice not in ["rock", "paper", "scissors"]:
+    print("Invalid choice! Please try again.")
+else:
+    print(f"You chose {player_choice}.")
